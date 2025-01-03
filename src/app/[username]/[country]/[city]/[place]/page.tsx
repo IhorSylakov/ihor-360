4bbdabda0f2cc40d5a58be189c5735003ba29cec
IhorSylakov <isylakov@gmail.com>
2025-01-03 22:57:30 +0100

    remove dashboard pages, remove photoId page

--- src/app/[username]/[country]/[city]/[place]/page.tsx
+++ src/app/[username]/[country]/[city]/[place]/page.tsx
@@ -1,5 +1,4 @@
-import Link from 'next/link';
-import Image from 'next/image';
+import PhotoViewer from '@/components/PhotoViewer';
 import { fetchPhotos } from '@/lib/firebaseHelpers';
 
 interface PlacePageProps {
@@ -18,28 +17,11 @@ export default async function PlacePage({ params }: PlacePageProps) {
   return (
     <div>
       <h1>{place} in {city}, {country}</h1>
-      <ul
-        style={{
-          display: 'grid',
-          gap: '15px',
-          listStyle: 'none',
-          gridTemplateColumns: 'repeat(auto-fill, minmax(220px ,1fr))'
-        }}
-      >
-        {photos.map((photo) => (
-          <li key={photo.id}>
-            <Link href={`/${username}/${country}/${city}/${place}/${photo.id}`}>
-              <Image
-                width={200}
-                height={100}
-                style={{ width: '100%', height: 'auto' }}
-                alt={`${photo.title}`}
-                src={photo.thumbnail ? photo.thumbnail : ''}
-              />
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <PhotoViewer
+        imageUrl={photos[0].panorama}
+        imagesList={photos}
+        containerHeight='400px'
+      />
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,8 @@
-import Link from 'next/link';
-// import styles from './page.module.css'
-
 export default function HomePage() {
   return (
     <>
       <section>
         <div>home page</div>
-        <div>
-          <Link href="/dashboard">dashboard</Link>
-        </div>
       </section>
     </>
   );
